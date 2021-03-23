dd10ca6ee3893397af843a9a5690d68756016f40
VoidFlower <bekran@bk.ru>
2021-03-24 02:14:20 +0300

    squash! Test application

--- src/main/java/com/workspace/server/config/SecurityConfiguration.java
+++ src/main/java/com/workspace/server/config/SecurityConfiguration.java
@@ -1,43 +1,43 @@
-package com.workspace.server.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SimpleSavedRequest;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .oauth2Login().and()
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
-                .authorizeRequests()
-                .antMatchers("/**/*.{js,html,css}").permitAll()
-                .antMatchers("/", "/api/user").permitAll()
-                .anyRequest().authenticated();
-    }
-
-    @Bean
-    public RequestCache refererRequestCache() {
-        return new HttpSessionRequestCache() {
-            @Override
-            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
-                String referrer = request.getHeader("referer");
-                if (referrer != null) {
-                    request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST", new SimpleSavedRequest(referrer));
-                }
-            }
-        };
-    }
-}
+//package com.workspace.server.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+//import org.springframework.security.web.savedrequest.RequestCache;
+//import org.springframework.security.web.savedrequest.SimpleSavedRequest;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@EnableWebSecurity
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .oauth2Login().and()
+//                .csrf()
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/**/*.{js,html,css}").permitAll()
+//                .antMatchers("/", "/api/user").permitAll()
+//                .anyRequest().authenticated();
+//    }
+//
+//    @Bean
+//    public RequestCache refererRequestCache() {
+//        return new HttpSessionRequestCache() {
+//            @Override
+//            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+//                String referrer = request.getHeader("referer");
+//                if (referrer != null) {
+//                    request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST", new SimpleSavedRequest(referrer));
+//                }
+//            }
+//        };
+//    }
+//}
