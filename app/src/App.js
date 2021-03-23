a6e5742853e66c4bffa94bda62dce976261871a3
VoidFlower <bekran@bk.ru>
2021-03-24 01:42:30 +0300

    Test application

---
+++ app/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
+import { CookiesProvider } from 'react-cookie';
+
+class App extends Component {
+  render() {
+    return (
+        <CookiesProvider>
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/groups' exact={true} component={GroupList}/>
+              <Route path='/groups/:id' component={GroupEdit}/>
+          </Switch>
+        </Router>
+        </CookiesProvider>
+
+    )
+  }
+}
+
+
+export default App;
