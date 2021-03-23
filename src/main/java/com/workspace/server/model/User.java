9b6b3d3a22136cdd54c8e5fb3b8cf1306a2ef883
VoidFlower <bekran@bk.ru>
2021-03-24 01:38:49 +0300

    Test application

---
+++ src/main/java/com/workspace/server/model/User.java
@@ -0,0 +1,20 @@
+package com.workspace.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+}
