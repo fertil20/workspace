5204b0aec7719c9d9cf6af000683786c116fc7c2
fertil <72992806+fertil20@users.noreply.github.com>
2021-03-25 22:11:49 +0300

    Авторизация всё ещё не переведена на фронт

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
@@ -10,5 +11,36 @@ function App() {
         </div>
     );
 }
+
+export default App;*/
+
+
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import GroupEdit from './GroupEdit';
+import { CookiesProvider } from 'react-cookie';
+import UserComponent from "./components/UserComponent";
+
+class App extends Component {
+    render() {
+        return (
+            <CookiesProvider>
+                <Router>
+                    <Switch>
+                        <Route path='/home' exact={true} component={Home}/>
+                        <Route path='/' exact={true} component={Home}/>
+                        <Route path='/login' exact={true} component={Home}/>
+                        <Route path='/users' exact={true} component={UserComponent}/>
+                        {/*<Route path='/users/:id' component={UserEdit}/>*/}
+                    </Switch>
+                </Router>
+            </CookiesProvider>
+
+        )
+    }
+}
+
 
 export default App;
